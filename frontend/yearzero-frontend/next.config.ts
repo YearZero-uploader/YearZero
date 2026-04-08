85f1469a05e12b419e6df485ebb4349930d6352c
DavidAvigdor1996 <davida1996@campus.technion.ac.il>
2026-04-08 04:34:05 +0300

    fixed pathing

--- frontend/yearzero-frontend/next.config.ts
+++ frontend/yearzero-frontend/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/YearZero",
+  // assetPrefix also helps ensure CSS/JS chunks load from the right place
+  assetPrefix: "/YearZero/",
   /* config options here */
 };
 
